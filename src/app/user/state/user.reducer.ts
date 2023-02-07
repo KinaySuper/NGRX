fa3b8c0a850651b23eef875a75a471f44155d4b0
kinay <yanou92@hotmail.com>
2023-02-07 09:51:17 -0800

    exercise start

--- src/app/user/state/user.reducer.ts
+++ src/app/user/state/user.reducer.ts
@@ -1,11 +1,16 @@
 import { createAction, createReducer, on } from "@ngrx/store";
+import { User } from "../user";
 
+export interface state{
+    users: userState;
+};
 export interface userState {
-    
+    hideUserName: boolean;
+    currentUser: User;
 }
 
-export const userReducer = createReducer(
-    { hideUserName : true},
+export const userReducer = createReducer<userState>(
+    { hideUserName : true} as userState,
     on(createAction('[User] Toggle User Name'), state =>{
         console.log('original state : ' + JSON.stringify(state));
         return{ 
